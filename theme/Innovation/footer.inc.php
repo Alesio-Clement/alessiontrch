610d891bfd3c729982d0ac8e37a8b7f38a8b12d1
Alesio Clement <clement.alesio1@gmail.com>
2024-10-16 00:25:03 +0300

    start-files

---
+++ theme/Innovation/footer.inc.php
@@ -0,0 +1,149 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); } 
+/****************************************************
+*
+* @File: 			footer.inc.php
+* @Package:		GetSimple
+* @Action:		Innovation theme for GetSimple CMS
+*
+*****************************************************/
+?>
+
+   <!--==============================
+        Footer Area
+    ==============================-->
+    <footer class="footer-wrapper footer-layout3 overflow-hidden bg-smoke">
+        <div class="container">
+            <div class="footer-menu-area">
+                <div class="row gy-3 justify-content-between">
+                    <div class="col-xxl-6 col-lg-7">
+                        <ul class="footer-menu-list">
+                            <li>
+                                <a href="<?php get_site_url(); ?>about-us/">
+                                    <span class="link-effect">
+                                        <span class="effect-1">ABOUT COMPANY</span>
+                                        <span class="effect-1">ABOUT COMPANY</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php get_site_url(); ?>our-projects/">
+                                    <span class="link-effect">
+                                        <span class="effect-1">PROJECTS</span>
+                                        <span class="effect-1">PROJECTS</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php get_site_url(); ?>our-services/">
+                                    <span class="link-effect">
+                                        <span class="effect-1">SERVICES</span>
+                                        <span class="effect-1">SERVICES</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php get_site_url(); ?>contact-us/">
+                                    <span class="link-effect">
+                                        <span class="effect-1">CONTACT US</span>
+                                        <span class="effect-1">CONTACT US</span>
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-xxl-6 col-lg-5 text-lg-end">
+                        <ul class="footer-menu-list">
+                            <li>
+                                <a href="<?php get_site_url(); ?>">
+                                    <span class="link-effect">
+                                        <span class="effect-1">BLOG</span>
+                                        <span class="effect-1">BLOG</span>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php get_site_url(); ?>faq/">
+                                    <span class="link-effect">
+                                        <span class="effect-1">FAQS</span>
+                                        <span class="effect-1">FAQS</span>
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-wrap">
+            <div class="container">
+                <div class="row gy-3 justify-content-between align-items-center">
+                    <div class="col-md-6">
+                        <div class="social-btn style3">
+                            <a href="#" target="_blank">
+                                <span class="link-effect">
+                                    <span class="effect-1"><i class="fab fa-facebook"></i></span>
+                                    <span class="effect-1"><i class="fab fa-facebook"></i></span>
+                                </span>
+                            </a>
+                            <a href="https://www.instagram.com/alessiontech/" target="_blank">
+                                <span class="link-effect">
+                                    <span class="effect-1"><i class="fab fa-instagram"></i></span>
+                                    <span class="effect-1"><i class="fab fa-instagram"></i></span>
+                                </span>
+                            </a>
+                            <a href="https://x.com/AlessionTech" target="_blank">
+                                <span class="link-effect">
+                                    <span class="effect-1"><i class="fab fa-twitter"></i></span>
+                                    <span class="effect-1"><i class="fab fa-twitter"></i></span>
+                                </span>
+                            </a>
+                            <a href="#">
+                                <span class="link-effect">
+                                    <span class="effect-1"><i class="fab fa-linkedin"></i></span>
+                                    <span class="effect-1"><i class="fab fa-linkedin"></i></span>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 align-self-center text-lg-end">
+                        <p class="copyright-text">Copyright © 2024
+                            <a href="www.alessiontech.com">AlessionTech</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scroll To Top -->
+    <div class="scroll-top">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 307.919;"></path>
+        </svg>
+    </div>
+
+	<script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
+	<div class="elfsight-app-19f155fe-b0e9-4b58-a1aa-dce7fac3d642" data-elfsight-app-lazy></div>
+
+    <!-- Jquery -->
+    <script src="<?php get_theme_url(); ?>/assets/js/vendor/jquery-3.6.0.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/slick.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/bootstrap.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/jquery.counterup.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/isotope.pkgd.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/gsap.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/twinmax.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/imageRevealHover.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/jarallax.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/jquery.marquee.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/jquery-ui.min.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/waypoints.js"></script>
+    <script src="<?php get_theme_url(); ?>/assets/js/wow.js"></script>
+
+    <!-- Main Js File -->
+    <script src="<?php get_theme_url(); ?>/assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ theme/Innovation/header.inc.php
@@ -0,0 +1,214 @@
+<?php if(!defined('IN_GS')){ die('you cannot load this page directly.'); }
+/****************************************************
+*
+* @File: 			header.inc.php
+* @Package:		GetSimple
+* @Action:		Innovation theme for GetSimple CMS
+*
+*****************************************************/
+?><!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="en" class="ie6"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="en" class="ie7"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="en" class="ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" > <!--<![endif]-->
+
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="AlessionTech is a leading digital agency specializing in web development, app development, digital marketing, creative design, and tech support. With over 8 years of experience and more than 50 successful projects, we provide innovative, tailored solutions to help businesses grow and succeed in the digital landscape. Our expert team is dedicated to delivering exceptional results and ensuring client satisfaction.">
+    <meta name="keywords" content="web development, app development, digital marketing, creative design, tech support, IT consultancy, SEO, UI/UX design, social media marketing, branding, cloud solutions, cybersecurity, graphic design, motion graphics, IT support, technology solutions">
+    <meta name="author" content="AlessionTech">
+    <meta property="og:title" content="AlessionTech - Innovating Your Digital Experience">
+    <meta property="og:description" content="AlessionTech offers expert web development, app development, digital marketing, creative design, and tech support services. With 8+ years of experience and 50+ successful projects, we deliver tailored solutions for business growth and digital success.">
+    <meta property="og:image" content="">
+    <meta property="og:url" content="https://alessiontech.com/">
+    <meta name="twitter:card" content="">
+    <meta name="twitter:title" content="AlessionTech - Innovating Your Digital Experience">
+    <meta name="twitter:description" content="AlessionTech provides top-tier web development, app development, digital marketing, creative design, and tech support. With over 8 years of experience and 50+ projects, we ensure exceptional results and client satisfaction.">
+    <meta name="twitter:image" content="">
+    <meta name="robots" content="INDEX,FOLLOW">
+
+    <title>AlessionTech</title>
+
+    <!-- Favicons - Place favicon.ico in the root directory -->
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php get_theme_url(); ?>../../../data/uploads/alession.png">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?php get_theme_url(); ?>../../../data/uploads/alession.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!--==============================
+	  Google Fonts
+	============================== -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&family=Unbounded:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+
+
+    <!--==============================
+	    All CSS File
+	============================== -->
+    <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/all.min.css">
+    <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/magnific-popup.min.css">
+    <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/slick.min.css">
+    <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/animate.min.css">
+    <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/imageRevealHover.css">
+    <!-- Theme Custom CSS -->
+    <link rel="stylesheet" href="<?php get_theme_url(); ?>/assets/css/style.css">
+
+</head>
+
+<body>
+ 
+    <!--==============================
+     Preloader
+    ==============================-->
+    <div class="preloader">
+        <div class="preloader-inner">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <!--==============================
+    Mobile Menu
+    ============================== -->
+    <div class="mobile-menu-wrapper">
+        <div class="mobile-menu-area">
+            <button class="menu-toggle"><i class="fas fa-times"></i></button>
+            <div class="mobile-logo">
+                <a href="<?php get_site_url(); ?>"><img src="<?php get_theme_url(); ?>../../../data/uploads/alession.png" width="60px"  height="60px" alt="Ovation"></a>
+            </div>
+            <div class="mobile-menu">
+                <ul>
+                    <li>
+                        <a href="<?php get_site_url(); ?>">HOME</a>
+                    </li>
+
+                    <li>
+                        <a href="<?php get_site_url(); ?>about-us/">ABOUT US</a>
+                    </li>
+
+                    <li>
+                        <a href="<?php get_site_url(); ?>our-services/">OUR SERVICES</a>
+                    </li>
+
+                    <li>
+                        <a href="<?php get_site_url(); ?>our-projects/">OUR PROJECTS</a>
+                    </li>
+
+
+                    <li>
+                        <a href="<?php get_site_url(); ?>">OUR BLOG</a>
+                    </li>
+
+                    
+                </ul>
+            </div>
+            <div class="sidebar-wrap">
+                <h6>1st Floor, Eagle House,</h6>
+                <h6>Nairobi,Kenya.</h6>
+            </div>
+            <div class="sidebar-wrap">
+                <h6><a href="mailto:info@alessiontech.com">info@alessiontech.com</a></h6>
+            </div>
+            <div class="social-btn style3">
+                <a href="https://www.facebook.com/">
+                    <span class="link-effect">
+                        <span class="effect-1"><i class="fab fa-facebook"></i></span>
+                        <span class="effect-1"><i class="fab fa-facebook"></i></span>
+                    </span>
+                </a>
+                <a href="https://instagram.com/">
+                    <span class="link-effect">
+                        <span class="effect-1"><i class="fab fa-instagram"></i></span>
+                        <span class="effect-1"><i class="fab fa-instagram"></i></span>
+                    </span>
+                </a>
+                <a href="https://twitter.com/">
+                    <span class="link-effect">
+                        <span class="effect-1"><i class="fab fa-twitter"></i></span>
+                        <span class="effect-1"><i class="fab fa-twitter"></i></span>
+                    </span>
+                </a>
+                <a href="https://dribbble.com/">
+                    <span class="link-effect">
+                        <span class="effect-1"><i class="fab fa-dribbble"></i></span>
+                        <span class="effect-1"><i class="fab fa-dribbble"></i></span>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+    <!--==============================
+	Header Area
+    ==============================-->
+    <header class="nav-header header-layout3 bg-white">
+        <div class="sticky-wrapper">
+            <!-- Main Menu Area -->
+            <div class="menu-area">
+                <div class="container-fluid">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-auto">
+                            <div class="header-logo">
+                                <a href="<?php get_site_url(); ?>"><img src="<?php get_theme_url(); ?>../../../data/uploads/alession.png" width="60px"  height="60px" alt="logo"></a>
+                            </div>
+                        </div>
+                        <div class="col-auto ms-auto">
+                            <nav class="main-menu d-none d-lg-inline-block">
+							<ul>
+                    <li>
+                        <a href="<?php get_site_url(); ?>">HOME</a>
+                    </li>
+
+                    <li>
+                        <a href="<?php get_site_url(); ?>about-us/">ABOUT US</a>
+                    </li>
+
+                    <li>
+                        <a href="<?php get_site_url(); ?>our-services/">OUR SERVICES</a>
+                    </li>
+
+                    <li>
+                        <a href="<?php get_site_url(); ?>our-projects/">OUR PROJECTS</a>
+                    </li>
+
+
+                    <li>
+                        <a href="<?php get_site_url(); ?>">OUR BLOG</a>
+                    </li>
+
+                    
+                </ul>
+                            </nav>
+                            <div class="navbar-right d-inline-flex d-lg-none">
+                                <button type="button" class="menu-toggle sidebar-btn">
+                                    <span class="line"></span>
+                                    <span class="line"></span>
+                                    <span class="line"></span>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="col-auto d-none d-lg-block">
+                            <div class="header-button">
+                                <a href="<?php get_site_url(); ?>contact-us/" class="btn bg-theme text-title">
+                                    <span class="link-effect">
+                                        <span class="effect-1">WORKS WITH US</span>
+                                        <span class="effect-1">WORKS WITH US</span>
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
